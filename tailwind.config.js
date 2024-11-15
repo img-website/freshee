c3604ce997ea2f5850adaa617aca41f2b9fb5f49
shivtejimg <135127037+shivtejimg@users.noreply.github.com>
2024-11-15 12:49:29 +0530

    project staup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'theme1': '#404721',
+        'theme2': '#D9D0D06E',
+        'theme3': '#82154F',
+        'theme4': '#96AE00',
+        'theme5': '#673C27',
+        'theme6': '##96AE00',
+
+
+      },
+    },
+  },
+  plugins: [],
+}
